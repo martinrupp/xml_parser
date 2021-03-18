80e37426c6542f07b422cbd1a3c299c375ebba90
Martin Rupp <mrupp@splicemachine.com>
2021-03-18 13:05:45 +0100

    fix test

--- src/test/java/XmlOSMTest.java
+++ src/test/java/XmlOSMTest.java
@@ -25,14 +25,12 @@ public class XmlOSMTest {
 
     @org.junit.Test
     public void simpleOsmTest() throws IOException, SAXException, ParserConfigurationException {
-//        try
-        {
-            Assert.assertEquals( "",
-                    printFile( "src/test/resources/simple_osm.xml" ));
-        }
-//        catch( Exception e )
-//        {
-//            Assert.fail(e.toString());
-//        }
+        Assert.assertEquals( "ways = [id=25291691, [275494491, 275503483, 275473051, 275463021, 275494491]], " +
+                        "nodes = [{id=36966060, lat=-79.406308, lon=0.314931}, {id=98257134, lat=-54.726099, lon=-63.795741}, " +
+                        "{id=248461592, lat=-62.159014, lon=-58.471936}, {id=248461594, lat=-62.478435, lon=-59.714083}, " +
+                        "{id=275462210, lat=-70.306450, lon=12.464890}, {id=275462254, lat=-70.302582, lon=12.522486}, " +
+                        "{id=275462346, lat=-70.817199, lon=-7.593200}, {id=8518223538, lat=-69.556953, lon=-72.872915}, " +
+                        "{id=8518223539, lat=-69.556684, lon=-72.878494}, {id=8518223540, lat=-69.556444, lon=-72.886820}]",
+                printFile( "src/test/resources/simple_osm.xml" ));
     }
 }
